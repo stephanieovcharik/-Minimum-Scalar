a0cf760e2b182233c8aa12fc3149f32bb5766a16
stephanieovcharik <stephanieovcharik@gmail.com>
2016-03-09 19:49:38 -0500

    Initial Commit

---
+++ Project1 Part2/src/MinimumScalar.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.util.Arrays;  
+import java.util.Collections;
+import java.util.Scanner;
+  
+  
+public class MinimumScalar {  
+	private static Scanner sc;
+    public static void main(String[] args) throws Exception {  
+    	sc = new Scanner(new File("input.txt")); 
+          
+        int totalCases = Integer.parseInt(sc.nextLine());  
+  
+        for (int cases = 1; cases <= totalCases; cases++) {  
+            int num = Integer.parseInt(sc.nextLine());  
+            String[] one = new String(sc.nextLine()).split(" ");  
+            String[] two = new String(sc.nextLine()).split(" ");  
+            int arr1[] = new int[num];  
+            for (int i = 0; i < num; i++) {  
+                arr1[i] = (Integer.parseInt(one[i]));
+            }  
+  
+            Integer arr2[] = new Integer[num];  
+            for (int i = 0; i < num; i++) {  
+                arr2[i] = (Integer.parseInt(two[i]));
+            }  
+  
+            Arrays.sort(arr1);
+            Arrays.sort(arr2, Collections.reverseOrder());  
+            
+            int answer = 0;
+            for (int i = 0; i < num; i++) 
+            	answer=answer + arr1[i] * arr2[i];
+                
+            System.out.println("Case #" + cases + ": " + answer );  
+  
+        }  
+        sc.close();  
+          
+    }
+    
+}
+
